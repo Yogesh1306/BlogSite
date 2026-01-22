9c33937ce14f23df338ef05ac79adbb218fbe526
Yogesh Joshi <work.yogeshjoshi@gmail.com>
2026-01-22 18:07:14 +0530

    feat: add Google login route and update HTTP methods for user profile routes

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import { changePassword, changeProfilePic, changeUsername, deleteUser, getCurrentUser, userLogin, userlogout, userRegister } from "../controllers/user.controllers.js";
+import { changePassword, changeProfilePic, changeUsername, deleteUser, getCurrentUser, googleLogin, userLogin, userlogout, userRegister } from "../controllers/user.controllers.js";
 import { jwtAuth } from "../middlewares/auth.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 
@@ -7,13 +7,14 @@ const router = Router()
 
 router.route("/register").post(userRegister)
 router.route("/login").post(userLogin);
+router.route("/google").post(googleLogin);
 
 //secure routes
 router.route("/").get(jwtAuth, getCurrentUser)
 router.route("/logout").post(jwtAuth,userlogout);
-router.route("/updatePassword").put(jwtAuth,changePassword);
-router.route("/updateUsername").put(jwtAuth,changeUsername);
-router.route("/updateProfilePic").put(jwtAuth,upload.single("profilePic"),changeProfilePic);
+router.route("/updatePassword").patch(jwtAuth,changePassword);
+router.route("/updateUsername").patch(jwtAuth,changeUsername);
+router.route("/updateProfilePic").patch(jwtAuth,upload.single("profilePic"),changeProfilePic);
 router.route("/removeUser").delete(jwtAuth, deleteUser)
 
 
